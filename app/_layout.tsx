e31207276bdbac01510e13d768d11b87211aa3ca
koniz-dev <20521498@gm.uit.edu.vn>
2025-11-13 01:49:26 +0000

    Wrap RootLayout in ErrorBoundary to enhance error handling for child components

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,14 +2,17 @@ import { PaperProvider } from 'react-native-paper';
 import { useColorScheme } from 'react-native';
 import { getTheme } from '@/constants/Theme';
 import { Stack } from 'expo-router';
+import { ErrorBoundary } from '@/components/ErrorBoundary';
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const theme = getTheme(colorScheme ?? null);
 
   return (
-    <PaperProvider theme={theme}>
-      <Stack screenOptions={{ headerShown: false }} />
-    </PaperProvider>
+    <ErrorBoundary>
+      <PaperProvider theme={theme}>
+        <Stack screenOptions={{ headerShown: false }} />
+      </PaperProvider>
+    </ErrorBoundary>
   );
 }
